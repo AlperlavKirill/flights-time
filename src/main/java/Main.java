bc1e90edcef179c9f23374ff20f0ec38b28474e3
AlperlavKirill <kirill@alperlav.com>
2021-11-23 20:34:25 +0300

    :tada: project started

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    private final static String JSON_FILE = "../resources/tickets.json";
+    private final static String DEPARTURE = "VVO";
+    private final static String ARRIVAL = "TLV";
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/model/Ticket.java
@@ -0,0 +1,154 @@
+package model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.time.LocalDate;
+
+public class Ticket {
+    private String origin;
+
+    @JsonProperty("origin_name")
+    private String originName;
+
+    private String destination;
+
+    @JsonProperty("destination_name")
+    private String destinationName;
+
+    @JsonProperty("departure_date")
+    @JsonFormat(pattern = "dd.MM.yy")
+    private LocalDate departureDate;
+
+    @JsonProperty("departure_time")
+    @JsonFormat(pattern = "HH:mm")
+    private LocalDate departureTime;
+
+    @JsonProperty("arrival_date")
+    @JsonFormat(pattern = "dd.MM.yy")
+    private LocalDate arrivalDate;
+
+    @JsonProperty("arrival_time")
+    @JsonFormat(pattern = "HH:mm")
+    private LocalDate arrivalTime;
+
+    private String carrier;
+    private int stops;
+    private int price;
+
+    public Ticket(String origin,
+                  String originName,
+                  String destination,
+                  String destinationName,
+                  LocalDate departureDate,
+                  LocalDate departureTime,
+                  LocalDate arrivalDate,
+                  LocalDate arrivalTime,
+                  String carrier,
+                  int stops,
+                  int price) {
+        this.origin = origin;
+        this.originName = originName;
+        this.destination = destination;
+        this.destinationName = destinationName;
+        this.departureDate = departureDate;
+        this.departureTime = departureTime;
+        this.arrivalDate = arrivalDate;
+        this.arrivalTime = arrivalTime;
+        this.carrier = carrier;
+        this.stops = stops;
+        this.price = price;
+    }
+
+    public Ticket() {
+
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public String getOriginName() {
+        return originName;
+    }
+
+    public void setOriginName(String originName) {
+        this.originName = originName;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public String getDestinationName() {
+        return destinationName;
+    }
+
+    public void setDestinationName(String destinationName) {
+        this.destinationName = destinationName;
+    }
+
+    public LocalDate getDepartureDate() {
+        return departureDate;
+    }
+
+    public void setDepartureDate(LocalDate departureDate) {
+        this.departureDate = departureDate;
+    }
+
+    public LocalDate getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(LocalDate departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public LocalDate getArrivalDate() {
+        return arrivalDate;
+    }
+
+    public void setArrivalDate(LocalDate arrivalDate) {
+        this.arrivalDate = arrivalDate;
+    }
+
+    public LocalDate getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(LocalDate arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    public String getCarrier() {
+        return carrier;
+    }
+
+    public void setCarrier(String carrier) {
+        this.carrier = carrier;
+    }
+
+    public int getStops() {
+        return stops;
+    }
+
+    public void setStops(int stops) {
+        this.stops = stops;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
